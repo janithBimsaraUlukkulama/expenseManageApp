2210a6713559c38013f8ff4bb6852dee41e71107
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2022-03-26 22:47:12 +0530

    up to header right icon

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from 'react-native';
+
+import { GlobalStyles } from './constants/styles';
+import AllExpensess from './screens/AllExpenses';
+import ManageExpensess from './screens/ManageExpensess';
+import RecentExpensess from './screens/RecentExpenses';
+import IconButton from './components/UI/IconButton';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensessOverview() {
+  return (
+    <BottomTabs.Navigator screenOptions={{
+      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      headerTintColor: 'white',
+      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      headerRight: ({ tintColor }) => (<IconButton icon="add" size={24} color={tintColor} onPress={() => { }} />)
+    }} >
+
+      <BottomTabs.Screen
+        name="RecentExpensess"
+        component={RecentExpensess}
+        options={{
+          title: 'Recent Expensess',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size }) => <Ionicons name='hourglass' size={size} color={color} />
+        }} />
+      <BottomTabs.Screen
+        name="AllExpensess"
+        component={AllExpensess}
+        options={{
+          title: 'All Expensess',
+          tabBarLabel: 'All',
+          tabBarIcon: ({ color, size }) => <Ionicons name='calendar' size={size} color={color} />
+
+        }} />
+
+    </BottomTabs.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='ExpensessOverView' component={ExpensessOverview} options={{ headerShown: false }} />
+          <Stack.Screen name='ManageExpensess' component={ManageExpensess} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
